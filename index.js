da14c083f0bbed9bcb655782ce503c3c87143ae1
Arnaldo Rodrigues <arnaldo.rodrigues@tuntscorp.com>
2014-10-01 14:34:35 -0300

    Renaming subscription to index

--- index.js
+++ index.js
@@ -1,38 +1,58 @@
+'use strict';
+
 var Firebase = require('firebase');
-var OrderHandler = require('./lib/order/OrderHandler.js');
-var UserState = require('./lib/handlers/UserState.js');
+var UserState = require('./lib/handlers/UserState');
+var WorkQueue = require('./lib/thirdparty/workqueue');
+
+var consultantModule = require('./lib/consultant');
+
+var Consultant = consultantModule.Entity;
 
 var fbDev = new Firebase('https://voppwishlist.firebaseio.com/');
-var fbPrd = new Firebase('https://vopp.firebaseio.com/');
 
 fbDev.auth("zDDGKRwrgxuJ2gpzMVK5I9h2gNDB4dFup7AW5nrh", function (error) {
-    if (error) {
-        console.log("Login Failed!", error);
-    } else {
-        console.log("Login Succeeded!");
-    }
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Login Succeeded!");
+    subscriptionUpdate();
+  }
 });
-
-/*fbPrd.auth("zhmVz7kUS7YBVUAbn4olhYaffVPA0n4nTCAb3WXe", function (error) {
- if (error) {
- console.log("Login Failed!", error);
- } else {
- console.log("Login Succeeded!");
- }
- });*/
-
-var journalRefs = [];
 
 var fbRoot = fbDev;
 
-/*fbRoot.child('users').on('child_added', function(userRef){
- console.log(userRef.child('email').val());
- });*/
+function subscriptionUpdate() {
+  var states = {};
 
-var orderHandler = new OrderHandler();
+  fbRoot.child('users').on('child_added', function (userRef) {
+    var childAdded = userRef.name();
+    console.log('Child added', childAdded);
+    states[childAdded] = new UserState(fbRoot.child('users').child(childAdded));
+  });
 
-var handlers = orderHandler.handlers;
+  var subscriptionUpdateQueue = fbRoot.child('queues').child('subscription-consultant-update').child('pending');
+  new WorkQueue(subscriptionUpdateQueue, function (data, whenFinished) {
 
-//state = new UserState(fbRoot.child('users').child('arnaldo_rodrigues@tuntscorp_com'));
-//state = new UserState(fbRoot.child('users').child('thiago_classen@tuntscorp_com'));
-state = new UserState(fbRoot.child('users').child('wesleyakio@tuntscorp_com'));
+    if(data.consultant && data.consultant.email) {
+
+      var username = data.consultant.email.replace(/\.+/g, '_');
+
+      if (states[username]) {
+        try {
+          var updatePromise = states[username].interfaces.consultant.update(new Consultant({uuid: data.consultant.uuid, subscriptionExpirationDate: data.consultant.newSubscriptionExpirationDate}));
+          updatePromise.catch(function (error) {
+            throw new Error(error);
+          });
+        } catch (e) {
+          fbRoot.child('queues').child('subscription-consultant-update').child('failed').push({message: data, error: e.message});
+        }
+
+      } else {
+        fbRoot.child('queues').child('subscription-consultant-update').child('failed').push({message: data, error: 'User does not have a state'});
+      }
+    } else {
+      fbRoot.child('queues').child('subscription-consultant-update').child('failed').push({message: data, error: 'Unable to identify the user'});
+    }
+    whenFinished();
+  });
+}
